d041af66495e0ac7438c1d92424a09a346f0352d
visola <visolad1@gmail.com>
2023-08-21 19:54:27 +0500

    .

---
+++ src/components/Categories.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Categories() {
+  const [activeIndex, setActiveIndex] = React.useState(0)
+  const categories = ['Все', 'Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытые']
+  const onClickCategory = (index) =>  {
+    setActiveIndex(index)
+  }
+
+    return (
+      <div className="categories">
+        <ul>
+          {categories.map((value, i) => (
+            <li key={value} onClick={() => onClickCategory(i)} className={activeIndex === i ? 'active' : ''}>{value}</li>
+          ))}
+        </ul>
+      </div>
+    )
+  }
+
+export default Categories
